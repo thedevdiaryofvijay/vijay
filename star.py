928560fcad83b954fb1234aa0d9837a43a5981a7
VIJJU1999 <vijaykumarkoneru13@gmaill.com>
2022-01-03 22:28:03 +0530

    ver2

--- star.py
+++ star.py
@@ -3,7 +3,7 @@ turtle.bgcolor("green")
 turtle.pensize(4)
 turtle.speed(0)
 for i in range (6):
-    for c in ["red","megente","blue","cyan"]
+    for c in ["red","megente","blue","cyan"]:
         turtle.color(c)
         turtle.circle(50)
         turtle.left(10)
